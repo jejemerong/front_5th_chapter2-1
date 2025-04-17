416239eee954f6153f76cc1ed08cc315649b3cd3
jejemerong <9708jjw@gmail.com>
2025-04-18 06:33:02 +0900

    feat: 상수, 타입 추가

---
+++ src/advanced/constants.ts
@@ -0,0 +1,13 @@
+// 세일 할인율
+export const LUCKY_DISCOUNT_RATE = 0.2;
+export const SUGGEST_DISCOUNT_RATE = 0.05;
+export const PRODUCT_DISCOUNT_RATE = {
+  p1: 0.1,
+  p2: 0.15,
+  p3: 0.2,
+  p4: 0.05,
+  p5: 0.25,
+};
+
+// 밀리초 변환
+export const SEC = 1000;
---
+++ src/advanced/types.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+}
+
+export interface CartItemType {
+  id: string;
+  stock: number;
+}
