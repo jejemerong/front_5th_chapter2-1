df06c0ccd65933a6777ddbe83763e034a27c8f2c
jejemerong <9708jjw@gmail.com>
2025-04-18 07:02:06 +0900

    deploy: gh-pages 설치 및 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-1/' : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.advanced.html'),
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+});
