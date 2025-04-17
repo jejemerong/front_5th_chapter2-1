fce45292a944535a993e445d41f4169bf03b43ed
jejemerong <9708jjw@gmail.com>
2025-04-18 06:49:15 +0900

    fix: 상수 사용

--- src/advanced/utils/calculateTotals.ts
+++ src/advanced/utils/calculateTotals.ts
@@ -1,3 +1,4 @@
+import { PRODUCT_DISCOUNT_RATE } from '../constants';
 import { CartItemType, Product } from '../types';
 
 export const calculateTotals = (
@@ -5,7 +6,7 @@ export const calculateTotals = (
   products: Product[]
 ): { roundedTotalAmount: number; discountRate: number; points: number } => {
   let totalAmount = 0;
-  let itemCnt = 0;
+  let itemCount = 0;
   let subTotal = 0;
 
   cart.forEach(({ id, stock }) => {
@@ -13,34 +14,17 @@ export const calculateTotals = (
     if (!prod) return;
     const itemTotal = prod.price * stock;
     subTotal += itemTotal;
-    itemCnt += stock;
+    itemCount += stock;
 
-    let disc = 0;
-    if (stock >= 10) {
-      switch (id) {
-        case 'p1':
-          disc = 0.1;
-          break;
-        case 'p2':
-          disc = 0.15;
-          break;
-        case 'p3':
-          disc = 0.2;
-          break;
-        case 'p4':
-          disc = 0.05;
-          break;
-        case 'p5':
-          disc = 0.25;
-          break;
-      }
-    }
-    totalAmount += itemTotal * (1 - disc);
+    const productDiscount =
+      stock >= 10 ? PRODUCT_DISCOUNT_RATE[id as keyof typeof PRODUCT_DISCOUNT_RATE] : 0;
+
+    totalAmount += itemTotal * (1 - productDiscount);
   });
 
   let discountRate = (subTotal - totalAmount) / subTotal || 0;
 
-  if (itemCnt >= 30) {
+  if (itemCount >= 30) {
     const bulkDisc = subTotal * 0.25;
     if (bulkDisc > subTotal - totalAmount) {
       totalAmount = subTotal * 0.75;
@@ -48,7 +32,6 @@ export const calculateTotals = (
     }
   }
 
-  // 화요일 추가 10% 할인
   if (new Date().getDay() === 2) {
     totalAmount *= 0.9;
     discountRate = Math.max(discountRate, 0.1);
